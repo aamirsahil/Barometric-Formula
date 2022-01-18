751d2349569a8f2198b6863af4f3fca8ed3c2d08
aamirsahil <60216436+aamirsahil@users.noreply.github.com>
2022-01-18 15:43:45 +0530

    Update navbar.js

--- Javascript/navbar.js
+++ Javascript/navbar.js
@@ -1,4 +1,4 @@
-var desc = ["Reality", "Idealization", "Descretization", "Geometric", "Mathematical"];
+var desc = ["Reality", "Idealization", "Descretization", "Geometric", "Algebraic"];
 for(let i = 1; i < 6; i++)
 {
     let text = document.getElementById("text" + i.toString());
