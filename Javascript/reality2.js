c51227cf2cd5d0555869694b81841c4bc6372a3f
aamirsahil <60216436+aamirsahil@users.noreply.github.com>
2022-02-02 12:45:19 +0530

    Update reality2.js

--- Javascript/reality2.js
+++ Javascript/reality2.js
@@ -6,9 +6,11 @@ if(value == 1)
     document.getElementById("img02").src = "images\\reality\\climber2.jpg";
     document.getElementById("img03").src = "images\\reality\\climber3.jpg";
     document.getElementById("img04").src = "images\\reality\\nosebleed.webp";
-    document.getElementById("text02").innerHTML = "What happens here is a balance between our blood pressure and atmospheric pressure.";
+    document.getElementById("text02").innerHTML = "Normally there is a balance between our blood pressure and atmospheric pressure.
+";
     document.getElementById("text03").innerHTML = "The blood pressure is controlled by our body.";
-    document.getElementById("text04").innerHTML = "This disparity in pressure causes the nosebleed experienced by the mountain climbers";
+    document.getElementById("text04").innerHTML = "A disparity in pressure can cause nosebleed, often experienced at higher altitudes
+.";
 }
 else if(value == 2)
 {
